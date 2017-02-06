b39da6bfa5c7550f65f036e2ea945d4c7f4a6ef4
Igor Bilan <igorvbilan@gmail.com>
2017-02-06 23:14:31 +0200

    init rails app

---
+++ bot/bot.rb
@@ -0,0 +1,9 @@
+#class Bot < SlackRubyBot::Bot
+  #command 'say' do |client, data, match|
+    #Rails.logger.info('match -------->>>')
+    #Rails.logger.info(match)
+    #Rails.logger.info(match['expression'])
+
+    #client.say(channel: data.channel, text: match['expression'])
+  #end
+#end
