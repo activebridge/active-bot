3938e32a812b7d5f78e7efcf6680fb0632cec576
Igor Bilan <igorvbilan@gmail.com>
2017-02-10 10:42:08 +0200

    add test_post_button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::API
   def default
     render json: 'Welcome to Active Accounting Bot.', status: 200
   end
+
+  def test_post_button
+    render json: 'test_post_button', status: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :application, only: :default do
+    get 'default'
+    post 'test_post_button'
+  end
+
   root to: 'application#default'
 end
