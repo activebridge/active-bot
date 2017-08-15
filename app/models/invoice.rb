9aa91eff7dcbc1a0a445773e750ce379e83c6e34
Igor Bilan <igorvbilan@gmail.com>
2017-08-15 16:22:04 +0300

    use private_class_method into Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,20 +9,22 @@ class Invoice < ApplicationRecord
 
   scope :last_months, -> { where(created_at: last_month_daterange) }
 
-  private
+  class << self
+    def last_month_daterange
+      today = Date.today
+      return ((last_business_day_for_current_month)..today.to_date.end_of_month) if today >= last_business_day_for_current_month && today <= today.to_date.end_of_month
+      (last_business_day_for_prev_month..today)
+    end
 
-  def self.last_month_daterange
-    today = Date.today
-    return ((last_business_day_for_current_month)..today.to_date.end_of_month) if today >= last_business_day_for_current_month && today <= today.to_date.end_of_month
-    (last_business_day_for_prev_month..today)
+    using DateRefinements
+    def last_business_day_for_current_month
+      @last_business_day_for_current_month ||= Date.last_business_day_for_current_month
+    end
+
+    def last_business_day_for_prev_month
+      @last_business_day_for_prev_month ||= Date.last_business_day(last_business_day_for_current_month << 1)
+    end
   end
 
-  using DateRefinements
-  def self.last_business_day_for_current_month
-    @last_business_day_for_current_month ||= Date.last_business_day_for_current_month
-  end
-
-  def self.last_business_day_for_prev_month
-    @last_business_day_for_prev_month ||= Date.last_business_day(last_business_day_for_current_month<<1)
-  end
+  private_class_method :last_business_day_for_current_month, :last_business_day_for_prev_month
 end
